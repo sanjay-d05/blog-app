7dbc69c634381df5099ea5b6c5008d758fb5185d
SANJAY D <148446978+sanjay-d05@users.noreply.github.com>
2025-06-26 22:27:13 +0530

    Update utils.js

--- server/config/utils.js
+++ server/config/utils.js
@@ -7,8 +7,8 @@ export const generateToken = async(userId,res) => {
     res.cookie("accessToken",token,{
         maxAge:1*24*60*60*1000,
         httpOnly:true,
-        sameSite:"strict",
-        secure:process.env.NODE_ENV !== 'development'
+        sameSite:"none",
+        secure:true
     });
 
     return token;
