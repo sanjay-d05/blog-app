a8ce0710ed4022e3eae136a709ecc15d8a4ef384
SANJAY D <148446978+sanjay-d05@users.noreply.github.com>
2025-06-26 21:46:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors({
   origin: [
     process.env.CLIENT_URI,
-    'https://blog-app-client-w5tg.onrender.com'
+    'https://blog-app-client-4j23.onrender.com'
   ] , 
   credentials:true
 }));
