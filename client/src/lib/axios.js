1572aaf3707164eb91249fd79f4770bc2d52e5b7
SANJAY D <148446978+sanjay-d05@users.noreply.github.com>
2025-07-02 20:29:41 +0530

    Update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-    baseURL:'http://localhost:8000/api',
+    baseURL:'https://blog-app-server-vd82.onrender.com/api',
     withCredentials:true
 });
